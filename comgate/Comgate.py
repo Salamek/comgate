952bdba99066601f8e701b1229737dca3e1efd67
Adam Schubert <adam.schubert@sg1-game.net>
2024-08-01 12:21:17 +0200

    feat(API): Update to include new required fields for create

--- comgate/Comgate.py
+++ comgate/Comgate.py
@@ -1,5 +1,7 @@
 import requests
+from typing import Optional
 from comgate.http.HttpClient import HttpClient
+from comgate.http.Response import Response
 from comgate.model.Payment import Payment
 from comgate.enum.CountryCodeEnum import CountryCodeEnum
 from comgate.enum.CurrencyCodeEnum import CurrencyCodeEnum
@@ -22,17 +24,17 @@ class Comgate:
         }
         self.http_client = HttpClient(self.session, base_data=base_data, base_url=self.api_gateway)
 
-    def create(self, payment: Payment):
+    def create(self, payment: Payment) -> Response:
         return self.http_client.post('create', payment.to_dict())
 
-    def get_status(self, trans_id: str):
+    def get_status(self, trans_id: str) -> Response:
         return self.http_client.get('status', {'transId': trans_id})
 
     def get_payment_methods(self,
                             lang: LanguageCodeEnum = LanguageCodeEnum.CS,
                             curr: CurrencyCodeEnum = CurrencyCodeEnum.CZK,
                             country: CountryCodeEnum = CountryCodeEnum.CZ
-                            ):
+                            ) -> Response:
 
         if country == CountryCodeEnum.ALL:
             raise Exception('Country must be specified, cannot use ALL')
@@ -45,7 +47,7 @@ class Comgate:
         }
         return self.http_client.get('methods', data)
 
-    def refund(self, trans_id: str, amount: str, curr: CurrencyCodeEnum = CurrencyCodeEnum.CZK, ref_id: str = None):
+    def refund(self, trans_id: str, amount: str, curr: CurrencyCodeEnum = CurrencyCodeEnum.CZK, ref_id: Optional[str] = None) -> Response:
         data = {
             'transId': trans_id,
             'amount': amount,
--- comgate/model/Payment.py
+++ comgate/model/Payment.py
@@ -1,66 +1,50 @@
-from typing import List
+import dataclasses
+from typing import List, Optional
 from comgate.enum.PaymentMethodCodeEnum import PaymentMethodCodeEnum
 from comgate.enum.CountryCodeEnum import CountryCodeEnum
 from comgate.enum.CurrencyCodeEnum import CurrencyCodeEnum
 from comgate.enum.LanguageCodeEnum import LanguageCodeEnum
 
 
+@dataclasses.dataclass
 class Payment:
-    def __init__(self,
-                 price: int,
-                 curr: CurrencyCodeEnum,
-                 label: str,
-                 ref_id: str,
-                 email: str,
-                 prepare_only: bool,
-                 payer_id: str = None,
-                 account: str = None,
-                 phone: str = None,
-                 name: str = None,
-                 lang: LanguageCodeEnum = LanguageCodeEnum.CS,
-                 pre_auth: bool = None,
-                 init_recurring: bool = None,
-                 verification: bool = None,
-                 embedded: bool = None,
-                 eet_report: bool = None,
-                 eet_data: dict = None,
-                 included_methods: List[PaymentMethodCodeEnum] = None,
-                 excluded_methods: List[PaymentMethodCodeEnum] = None,
-                 country: CountryCodeEnum=CountryCodeEnum.ALL):
-        self.price = price
-        self.curr = curr
-        self.label = label
-        self.ref_id = ref_id
-        self.email = email
-        self.payer_id = payer_id
-        self.account = account
-        self.phone = phone
-        self.name = name
-        self.lang = lang
-        self.prepare_only = prepare_only
-        self.pre_auth = pre_auth
-        self.init_recurring = init_recurring
-        self.verification = verification
-        self.embedded = embedded
-        self.eet_report = eet_report
-        self.eet_data = eet_data
-        self.included_methods = included_methods
-        self.excluded_methods = excluded_methods
-        self.country = country
+    price: int
+    curr: CurrencyCodeEnum
+    label: str
+    ref_id: str
+    email: str
+    phone: str
+    full_name: str
+    prepare_only: bool
+    lang: LanguageCodeEnum = LanguageCodeEnum.CS
+    country: CountryCodeEnum = CountryCodeEnum.ALL
+    payer_id: Optional[str] = None
+    account: Optional[str] = None
+    name: Optional[str] = None
+    pre_auth: Optional[bool] = None
+    init_recurring: Optional[bool] = None
+    verification: Optional[bool] = None
+    embedded: Optional[bool] = None
+    eet_report: Optional[bool] = None
+    eet_data: Optional[dict] = None
+    included_methods: Optional[List[PaymentMethodCodeEnum]] = None
+    excluded_methods: Optional[List[PaymentMethodCodeEnum]] = None
 
-        self.method = '+'.join([i.value for i in included_methods])
-        if excluded_methods:
-            self.method += '-' + '-'.join([e.value for e in excluded_methods])
+    def to_dict(self) -> dict:
+        method = '+'.join([i.value for i in self.included_methods]) if self.included_methods else ''
+        if self.excluded_methods:
+            method += '-' + '-'.join([e.value for e in self.excluded_methods])
 
-    def to_dict(self):
         return {
             'price': self.price,
             'curr': self.curr.value,
             'label': self.label,
             'refId': str(self.ref_id),
-            'method': self.method,
+            'method': method,
             'country': self.country.value,
             'lang': self.lang.value,
             'email': self.email,
+            'phone': self.phone,
+            'fullName': self.full_name,
             'prepareOnly': 'true' if self.prepare_only else 'false',
         }
